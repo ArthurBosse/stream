38d9bd81d2e89b90534caa48cc3f16e6a3e61ddd
ArthurPhyto <hello@arthur-cleron.fr>
2025-01-17 14:45:50 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({
         <link rel="icon" href="/application-de-television-en-streaming.ico" sizes="any" />
       </head>
       <body className={`${inter.className} bg-gradient-to-b from-black to-gray-900 min-h-screen text-white`}>
+      <script type='text/javascript' src='//pl25594665.profitablecpmrate.com/69/d1/5b/69d15b8d926a631c012951646752ba58.js'></script>
         <GoogleAnalytics />
         <TelegramBanner />
         <Header />
