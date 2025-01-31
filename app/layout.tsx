6531b90c9cb37dd5cff25521ee8700a63433fb9f
ArthurPhyto <hello@arthur-cleron.fr>
2025-01-31 23:18:18 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,28 @@ export default function RootLayout({
     <html lang="fr">
       <head>
         <link rel="icon" href="/application-de-television-en-streaming.ico" sizes="any" />
+        {/* Yandex.Metrika counter */}
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(99747832, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true
+            });
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/99747832" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+          </div>
+        </noscript>
       </head>
       <body className={`${inter.className} bg-gradient-to-b from-black to-gray-900 min-h-screen text-white`}>
         <GoogleAnalytics />
